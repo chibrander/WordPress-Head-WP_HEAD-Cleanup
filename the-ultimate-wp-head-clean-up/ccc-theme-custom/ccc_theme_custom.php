6cecb0c7e43e5c26a2cc9a1c3a0ad8bdaae8c845
chicagocomputerclasses <info@chicagocomputerclasses.com>
2016-03-06 19:45:25 -0600

    Added files via upload

---
+++ the-ultimate-wp-head-clean-up/ccc-theme-custom/ccc_theme_custom.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package CCC Theme Custom
+ * @version 1.0
+ */
+/*
+Plugin Name: CCC Theme Custom
+Plugin URI: http://www.chicagocomputerclasses.com/
+Description: Custom functions for EnFold Theme
+Author: Kevin
+Version: 1.0
+Author URI: http://www.chicagocomputerclasses.com/
+*/
+
+
+?>
---
+++ the-ultimate-wp-head-clean-up/ccc-theme-custom/readme.txt
@@ -0,0 +1,11 @@
+=== CCC Theme Custom ===
+Contributors: Kevin
+Requires at least: 3.0
+Stable tag: 1.0
+Tested up to: 4.1
+
+Custom finctions for Enfold theme.
+
+== Description ==
+
+Just so that we can roll with updates faster.
